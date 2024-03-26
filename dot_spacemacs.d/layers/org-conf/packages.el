3ac8c75a69d218968e913bd34da7cddbd6ed6320
jw-cpnet <jian.wang@cpnet.io>
2024-03-26 09:58:20 +0800

    refactor org-jira function advice; return to original window after execution

--- dot_spacemacs.d/layers/org-conf/packages.el
+++ dot_spacemacs.d/layers/org-conf/packages.el
@@ -193,52 +193,25 @@
     (add-to-list 'org-tempo-keywords-alist templ))
 
   (require 'org-jira)
-
-  (defvar org-jira-browse-issue-from-agenda nil
-    "Non-nil if `org-jira-browse-issue' was called from an `org-agenda' buffer.")
-
-  (defun org-jira-browse-issue-advice (orig-fun &rest args)
-    "Advice for `org-jira-browse-issue'.
+  (defun org-jira-from-agenda (orig-fun &rest args)
+      "Advice for org-jira functions.
   If called from an `org-agenda' buffer and the cursor is on an agenda entry,
   visit the corresponding org buffer, call the original function, and then
   switch back to the `org-agenda' buffer with the cursor at the same position."
-    (let ((marker (when (and (eq major-mode 'org-agenda-mode)
-                            (get-text-property (point) 'org-marker))
-                    (get-text-property (point) 'org-marker))))
-      (if marker
-          (progn
-            (setq org-jira-browse-issue-from-agenda t)
-            (save-excursion
-              (org-agenda-goto marker)
-              (message "Visiting org item: %s" (org-get-heading t t))
-              (apply orig-fun args)))
-        (apply orig-fun args))))
-
-  (advice-add 'org-jira-browse-issue :around #'org-jira-browse-issue-advice)
-
-
-  (defvar org-jira-refresh-issue-from-agenda nil
-    "Non-nil if `org-jira-refresh-issue' was called from an `org-agenda' buffer.")
-
-  (defun org-jira-refresh-issue-advice (orig-fun &rest args)
-    "Advice for `org-jira-refresh-issue'.
-  If called from an `org-agenda' buffer and the cursor is on an agenda entry,
-  visit the corresponding org buffer, call the original function, and then
-  switch back to the `org-agenda' buffer with the cursor at the same position."
-    (let ((marker (when (and (eq major-mode 'org-agenda-mode)
-                            (get-text-property (point) 'org-marker))
-                    (get-text-property (point) 'org-marker))))
-      (if marker
-          (progn
-            (setq org-jira-refresh-issue-from-agenda t)
-            (save-excursion
+      (let ((marker (when (and (eq major-mode 'org-agenda-mode)
+                              (get-text-property (point) 'org-marker))
+                      (get-text-property (point) 'org-marker))))
+        (if marker
+            (save-window-excursion
               (org-agenda-goto marker)
               (message "Visiting org item: %s"
                       (org-get-heading t t))
-              (apply orig-fun args)))
-        (apply orig-fun args))))
+              (apply orig-fun args))
+          (apply orig-fun args))))
 
-  (advice-add 'org-jira-refresh-issue :around #'org-jira-refresh-issue-advice)
+  (advice-add 'org-jira-copy-current-issue-key :around #'org-jira-from-agenda)
+  (advice-add 'org-jira-refresh-issue :around #'org-jira-from-agenda)
+  (advice-add 'org-jira-browse-issue :around #'org-jira-from-agenda)
 
   ;; Files
   (setq org-directory "~/Dropbox/org-new")
