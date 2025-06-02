e3e9a31658032c6f96194ff9ea540d1037d8636f
Jian Wang <jian.wang@cpnet.io>
2025-06-02 19:48:20 +0800

    update dot spacemacs and ssh env script

--- dot_spacemacs.d/scripts/executable_update_ssh_auth_sock.sh
+++ dot_spacemacs.d/scripts/executable_update_ssh_auth_sock.sh
@@ -2,11 +2,13 @@
 new_auth_sock=$(echo $SSH_AUTH_SOCK)
 
 # Prioritize non-gcr agent if available
-if [[ "$new_auth_sock" != "/run/user/1001/gcr/ssh" && -S "$new_auth_sock" ]]; then
+if [[ -n "$new_auth_sock" && "$new_auth_sock" != "/run/user/1000/gcr/ssh" && -S "$new_auth_sock" ]]; then
 	sed -i.bak -E "s|^SSH_AUTH_SOCK=.*|SSH_AUTH_SOCK=$new_auth_sock|" ~/.spacemacs.d/.spacemacs.env
-elif [[ -S "/run/user/1001/gcr/ssh" ]]; then # Fallback to gcr agent if it exists
-	sed -i.bak -E "s|^SSH_AUTH_SOCK=.*|SSH_AUTH_SOCK=/run/user/1001/gcr/ssh|" ~/.spacemacs.d/.spacemacs.env
+elif [[ -S "/run/user/1000/gcr/ssh" ]]; then # Fallback to gcr agent if it exists
+	sed -i.bak -E "s|^SSH_AUTH_SOCK=.*|SSH_AUTH_SOCK=/run/user/1000/gcr/ssh|" ~/.spacemacs.d/.spacemacs.env
+elif [[ -S "/run/user/1000/gnupg/S.gpg-agent.ssh" ]]; then # Fallback to GPG agent SSH socket
+	sed -i.bak -E "s|^SSH_AUTH_SOCK=.*|SSH_AUTH_SOCK=/run/user/1000/gnupg/S.gpg-agent.ssh|" ~/.spacemacs.d/.spacemacs.env
 else
-	echo "Error: No valid SSH_AUTH_SOCK found." >&2
-	exit 1
+	echo "Warning: No valid SSH_AUTH_SOCK found. Using GPG agent as default." >&2
+	sed -i.bak -E "s|^SSH_AUTH_SOCK=.*|SSH_AUTH_SOCK=/run/user/1000/gnupg/S.gpg-agent.ssh|" ~/.spacemacs.d/.spacemacs.env
 fi
