2d6b10a3ebae0bf2575875648ea2a3453709add0
Jian Wang <jian.wang@cpnet.io>
2025-06-04 13:50:28 +0800

    uid 1001 -> 1000

--- dot_spacemacs.d/scripts/executable_update_ssh_auth_sock.sh
+++ dot_spacemacs.d/scripts/executable_update_ssh_auth_sock.sh
@@ -2,10 +2,10 @@
 new_auth_sock=$(echo $SSH_AUTH_SOCK)
 
 # Prioritize non-gcr agent if available
-if [[ "$new_auth_sock" != "/run/user/1001/gcr/ssh" && -S "$new_auth_sock" ]]; then
+if [[ "$new_auth_sock" != "/run/user/1000/gcr/ssh" && -S "$new_auth_sock" ]]; then
 	sed -i.bak -E "s|^SSH_AUTH_SOCK=.*|SSH_AUTH_SOCK=$new_auth_sock|" ~/.spacemacs.d/.spacemacs.env
-elif [[ -S "/run/user/1001/gcr/ssh" ]]; then # Fallback to gcr agent if it exists
-	sed -i.bak -E "s|^SSH_AUTH_SOCK=.*|SSH_AUTH_SOCK=/run/user/1001/gcr/ssh|" ~/.spacemacs.d/.spacemacs.env
+elif [[ -S "/run/user/1000/gcr/ssh" ]]; then # Fallback to gcr agent if it exists
+	sed -i.bak -E "s|^SSH_AUTH_SOCK=.*|SSH_AUTH_SOCK=/run/user/1000/gcr/ssh|" ~/.spacemacs.d/.spacemacs.env
 else
 	echo "Error: No valid SSH_AUTH_SOCK found." >&2
 	exit 1
